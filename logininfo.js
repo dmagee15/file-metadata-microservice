ad2d8659337acba0eb580f45efea9f5e00fcd885
System <none>
2017-09-01 00:56:20 +0000

    👶👡 Updated with Glitch

--- logininfo.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose'),
-Schema = mongoose.Schema;
-
-var template = new Schema({
-  username: String,
-  password: String
-});
-
-module.exports = mongoose.model('template', template);
--- server.js
+++ server.js
@@ -6,25 +6,33 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var multer = require('multer');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://test:test@ds161913.mlab.com:61913/testinglogin');
 
 app.use(bodyparser.urlencoded({'extended': false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/views', express.static(process.cwd() + '/views'));
 
-
 app.route('/')
     .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
     })
 
-app.post('/input', function(req,res){
-  var name = req.body.username;
-  console.log(name);
+var storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, 'public/');
+    },
+    filename: function(req, file, callback){
+        callback(null, Date.now() + "-" + file.originalname)
+    }
+});
+
+var upload = multer({storage:storage}).single('file');
+
+
+app.post('/', upload, function(req,res){
+  var name = req.file.size;
+  console.log(req.file);
+  res.end();
 });
 
 app.listen(process.env.PORT, function () {
